5da1b5c8dbf46c47f5dd9c819535d1d76e85d9be
Mark Soper <masoper@gmail.com>
2013-03-16 00:22:21 -0400

    ready to load regions

---
+++ www/index.js
@@ -0,0 +1,14 @@
+
+(function(BRACKET) {
+
+  var ready = function() {
+    var region;
+    for (var regionName in BRACKET.regions) {
+      region = BRACKET.regions[regionName];
+      
+    }
+  };
+
+  $(ready);
+
+})(BRACKET);
