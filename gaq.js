b3b754ce8f1fa36919a2a4bcee4bedf707889b68
Mark Soper <masoper@gmail.com>
2013-03-19 11:14:22 -0400

    bracketwise score

--- gaq.js
+++ gaq.js
@@ -1,8 +1,9 @@
 
 
 
+
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-39369294-1']);
+  _gaq.push(['_setAccount', 'UA-39369294-2']);
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -10,4 +11,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
